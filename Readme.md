e1be4a041df5c9d83383e14b2c5b296680fc2508
Abubakr <abubakr9423@gmail.com>
2025-09-17 17:53:54 +0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,47 @@
+# Readme for 5 of JS 
+![image](./create%20an%20image%20for%20.png)
+
+## Table of content
+- string
+- metods
+- number
+
+# String
+we have 3 type of string frst one 
+```js 
+let doubleQuotes="hello"
+```
+second one
+```js
+let sinfleQoutes='hello'
+```
+
+third one 
+```js
+let backticks=`hello ${2+2` // hello 4
+```
+
+# method 
+- char At(),at()
+- toString()
+- concat()
+- trim()
+- includes()
+- index(),lasrindexOf()
+- replace(),slice()
+- split()
+- toLowerCase(),toUpperCase()
+
+
+# number
+Math
+.floor // if we habe 1.5 it make it 1
+.ceil // if we have 1.5 it make it 2
+.round // if we have 1.4 it make it 1 and if we have 1.5 he make it 2
+.abs //make negative number positive
+.max //find max number
+.min // find min number 
+.pow // if we have a b it make a to the power of a
+.sqrt // it say to us square of number
+.random  // i dont understand 
+.isNaN // i dont understand 
---
+++ main.js
@@ -0,0 +1,49 @@
+// task 1
+// function strLegth(a,cnt) {
+//     if (a.length === 0 || cnt <= 0) {
+//         return str1
+//     }
+//     let last=a[a.length-1]
+//     return a + last.repeat(cnt)
+// }
+// console.log(strLegth("hello",100));
+
+// task 2
+// function str(a) {
+//     const lastLetter = a[a.length - 1];
+//     const str2=a[0]
+//     return str2+lastLetter
+// }
+// console.log(str("ganesh"));
+// console.log(str("shiva"));
+// console.log(str("vishnu"));
+
+// task 3
+// function toint(a) {
+//     return Number(a);
+// }
+// function tostr(a2) {
+//     return a2+""
+// }
+// console.log(tostr(123));
+
+// task 4
+// function isPlural(a) {
+//     const lastLetter = a[a.length - 1];
+//     const str2=a[0]
+//     if (lastLetter=="s" && str2=="c") {
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// console.log(isPlural("changes"));
+// console.log(isPlural("dudes"));
+
+// task 5
+// function rev(a) {
+// const transformed = a.split("").reverse().join("").toUpperCase()
+// return transformed
+// }
+// console.log(rev("abc"));
